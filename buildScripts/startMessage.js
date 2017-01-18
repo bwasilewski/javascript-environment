b4f63ab35777313d3eb45bcf6909563e09e4a858
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2017-01-18 10:31:55 -0600

    Set up unit tests

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,9 @@
 import chalk from 'chalk';
 
-console.log(chalk.green('*** starting app in dev mode ***')); // eslint-disable-line no-console
+printMessage('*** Javascript Development Environment ***');
+printMessage('*** Author: Benjamin Wasilewski <benwasilewski@gmail.com> ***'); // eslint-disable-line no-console
+printMessage('*** starting app in dev mode ***'); // eslint-disable-line no-console
+
+function printMessage(msg) {
+  console.log(chalk.green(msg));   // eslint-disable-line no-console
+}
---
+++ buildScripts/testSetup.js
@@ -0,0 +1,7 @@
+// this file is not transpiled so we must use CommonJS and ES5
+
+// Register Babel to transpile before our tests run
+require('babel-register')();
+
+// Disable webpack features that Mocha doesn't understand
+require.extensions['.css'] = function () {};
