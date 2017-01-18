e99636f9c216e9949b448e5f1ad99d984e4d9b8e
Benjamin Wasilewski <benwasilewski@gmail.com>
2017-01-18 11:29:58 -0600

    continuous integration work in progress

--- src/index.test.js
+++ src/index.test.js
@@ -4,12 +4,18 @@ import fs from 'fs';
 
 describe('Our first test', () => {
   it('should pass', () => {
-    expect(true).to.equal(false);
+    expect(true).to.equal(true);
   })
 })
 
 describe('index.html', () => {
-  it('should say hello', () => {
+  it('should say hello', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function (error, window) {
+      const h1 = window.document.getElementsByTagName('h1')[0];
+      expect(h1.innerHTML).to.equal('Hello World!');
+      done();
+      window.close();
+    });
   })
 })
