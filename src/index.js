2c31b65803cf6e5e31207814a259e56886d9f850
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2017-01-18 22:30:49 -0600

    wrapping up http request logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // module imports
-import {getUsers} from './api/userApi';
+import $ from 'jquery'
+import {getUsers, deleteUser} from './api/userApi'
 
 // style imports
 import './index.css';
@@ -10,13 +11,26 @@ getUsers().then(result => {
 
   result.forEach(user => {
     usersBody += `<tr>
-      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td><a href="#" data-id="${user.id}" class="delete-user">Delete</a></td>
       <td>${user.id}</td>
-      <td>${user.fname}</td>
-      <td>${user.lname}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
       <td>${user.email}</td>
       </tr>`;
   });
 
   global.document.getElementById('users').innerHTML = usersBody;
+
+  // const deleteLinks = global.document.getElementsByClassName('delete-user');
+  const deleteLinks = $('.delete-user');
+
+  deleteLinks.on('click', ev => {
+    const $target = $(ev.currentTarget);
+    const $row = $target.parents('tr')
+
+    ev.preventDefault();
+
+    deleteUser($target.attr('data-id'));
+    $row.remove();
+  })
 })
